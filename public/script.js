eda721b9e14052131544562d27dfefd6c5b20f0e
iyo-27 <iyo.nina27@gmail.com>
2023-06-19 15:35:44 +0900

    TypeScriptの勉強-Bootstrap編

---
+++ public/script.js
@@ -0,0 +1,28 @@
+let dom = document.querySelector('#root');
+
+let element = React.createElement(
+    'div', {}, [
+        React.createElement(
+            'h2', {}, "Hellow!"
+        ),
+        React.createElement(
+            'h3', {className:'alert alert-primary'}, "React sample page."
+        ),
+        React.createElement(
+            'ul', {className:'list-group'}, [
+                React.createElement(
+                    'li', {className:'list-group-item'}, "First item."
+                ),
+                React.createElement(
+                    'li', {className:'list-group-item'}, "Second item."
+                ),
+                React.createElement(
+                    'li', {className:'list-group-item'}, "Third item."
+                ),
+            ]
+        ),
+
+    ]
+)
+
+ReactDOM.render(element, dom)
