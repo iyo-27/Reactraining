da763ea39d5111442e72834d33d233610d30a6bc
iyo-27 <iyo.nina27@gmail.com>
2023-07-03 14:37:45 +0900

    ステートを使ってみよう！！

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends Component{
   constructor(props){
     super()
     this.title = props.title
+    this.state = {
+      msg:"Hello Component",
+    }
   }
 
   render(){
@@ -15,9 +18,8 @@ class App extends Component{
         <h1 className='bg-primary text-white display-4'>React</h1>
         <div className='container'>
           <p className='subtitle'>{this.title}</p>
-          <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-          <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-          <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+          <p className='alert alert-warning'>{this.state.msg}</p>
+          <p className='alert alert-dark'>{this.props.msg}</p>
         </div>
       </div>
     );
