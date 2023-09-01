8ae5111271ae60bb82d162104537031002b398a1
iyo.nina27@gmail.com <iyo.nina27@gmail.com>
2023-09-02 01:55:02 +0900

    20230902_深夜、怖いからコミット

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
 import React, {Component, useEffect, useState} from 'react';
 import './App.css';
-
-function useTax(t1, t2){
-  const [price, setPrice] = useState(1000)
-  const [tx1] = useState(t1)
-  const [tx2] = useState(t2)
-
-  const tax = ()=>{
-    return Math.floor(price * (1.0 + tx1 / 100))
-  }
-
-  const reduced = ()=>{
-    return Math.floor(price * (1.0 + tx2 / 100))
-  }
-
-  return [price, tax, reduced, setPrice]
-}
-
-function AlertMessage(props){
-  const [price, tax, reduced, setPrice] = useTax(10, 8)
-
-  const DoChange = (e)=>{
-    let p = e.target.value
-    setPrice(p)
-  }
-
-  return <div className='alert alert-primary h5'>
-    <h5 className='h5'>通常税率: {tax()}円.</h5>
-    <h5 className='h5'>軽減税率: {reduced()}円.</h5>
-    <div className='form-group'>
-      <label className='form-group-label'>Price</label>
-      <input type='number' className='form-control' onChange={DoChange} value={price} />
-    </div>
-  </div>
-}
+import MemoPage from './memo/MemoPage'
 
 function App(){
 
@@ -41,8 +8,8 @@ function App(){
     <div>
       <h1 className='bg-primary text-white display-4'>React</h1>
       <div className='container'>
-        <h4 className='my-3'>Hooks sample</h4>
-        <AlertMessage />
+        <h4 className='my-3'>Memo</h4>
+        <MemoPage />
       </div>
     </div>
   )
